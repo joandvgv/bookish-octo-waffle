de4f2e681db6595fe6abb5cf31a1c675e12afaf5
Joan <joandv.gil@gmail.com>
2022-06-23 22:38:07 -0300

    Fix app test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,14 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "App";
+import ThemeContextProvider from "context/ThemeContext";
 
 describe("App", () => {
   it("renders theme correctly upon changing state", async () => {
-    const { getByText } = render(<App />);
+    const { getByText } = render(
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    );
     const container = screen.getByTestId("container");
 
     fireEvent.click(getByText("Toggle Theme"));
